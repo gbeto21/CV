ee2806d5bcce4d8d478b0825055feff7d8f6554e
Alberto González Balderas <albertoglezb@hotmail.com>
2019-09-23 18:12:57 -0500

    Se completó el mecanismo para mostrar y ocultar el panel en dispositivos pequeños.

--- src/scripts/curriculum.js
+++ src/scripts/curriculum.js
@@ -1,22 +1,64 @@
 const photo = document.querySelector('#photoContainer');
 const headerModal = document.querySelector('#photoContainerModal');
+const panel = document.querySelector("#panel")
+const main = document.querySelector("#main")
+const buttonClose = document.querySelector("#buttonClose")
 const smallDevice = window.matchMedia('screen and (max-width: 320px) and (min-width: 0px)')
+
+const eventClick = 'click'
+const none = 'none'
+const block = 'block'
+
+var showingPanel = false
 
 smallDevice.addListener(validateSmallDevice)
 validateSmallDevice(smallDevice)
 
-function validateSmallDevice(event){
-    if(event.matches)
-        photo.addEventListener('click', showModal)
-    else
-        photo.removeEventListener('click', showModal)
+function validateSmallDevice(event) {
+    if (event.matches) {
+        photo.addEventListener(eventClick, validateShowModal)
+        buttonClose.addEventListener(eventClick, hideModal)
+        validateHidePanel()
+    }
+
+    else {
+        photo.removeEventListener(eventClick, validateShowModal)
+        buttonClose.removeEventListener(eventClick, hideModal)
+    }
 }
 
-function showModal(){
-    
-    if(headerModal.classList.contains('active'))
-        headerModal.classList.remove('active')
-    else
-        headerModal.classList.add('active');
-    debugger
+function validateHidePanel() {
+
+    if (showingPanel == false)
+        hidePanel()
+}
+
+function hidePanel() {
+
+    main.style.display = block
+    panel.style.display = none
+    headerModal.style.display = none
+}
+
+function showPanel(){
+
+    main.style.display = none
+    panel.style.display = block
+    headerModal.style.display = block
+}
+
+function validateShowModal() {
+
+    if (showingPanel == false) {
+        showPanel()
+        showingPanel = true;
+    }
+}
+
+function hideModal() {
+
+    if (showingPanel) {
+        hidePanel()
+        showingPanel = false;
+    }
 }
